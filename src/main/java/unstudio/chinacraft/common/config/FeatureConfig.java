2c1552bc9c3d84c5f71620ca6b979a762d0e5cee
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-03 14:21:41 +0100

    Add configs to change frequency of Ore Generation

--- src/main/java/unstudio/chinacraft/common/config/FeatureConfig.java
+++ src/main/java/unstudio/chinacraft/common/config/FeatureConfig.java
@@ -16,6 +16,11 @@ public class FeatureConfig {
     public static boolean enableDoubleJump;
     public static boolean enableAdvancedSericulture;
     public static boolean enableShiftShootFirecrackers;
+    public static int copperOreFrequency;
+    public static int tinOreFrequency;
+    public static int silverOreFrequency;
+    public static int jadeOreFrequency;
+    public static int marbleOreFrequency;
 
     private Configuration c = ChinaCraft.getMainConfig();
 
@@ -27,5 +32,10 @@ public class FeatureConfig {
         ItemBombInRedPackerExplosion = c.getBoolean("ItemBombInRedPackerExplosion","Bomb",true,"While The Red Pakcer Witch Include a Bomb Was Opened , It Will Explose If True");
         enableDoubleJump = c.getBoolean("enableDoubleJump","Common",true,"If enable double jump of night clothes");
         enableAdvancedSericulture = c.getBoolean("EnableAdvancedSericulture", "Sericulture", true, "Set this to true will enable the advanced Sericulture, which means that the silkworms will have extra attributes, such like the productivity, speed and fertility. This is more complicated but is also more interesting to play with.");
+        copperOreFrequency = c.get("ores", "CopperFrequency", 20).getInt();
+        tinOreFrequency = c.get("ores", "TinFrequency", 10).getInt();
+        silverOreFrequency = c.get("ores", "SilverFrequency", 4).getInt();
+        jadeOreFrequency = c.get("ores", "JadeFrequency", 4).getInt();
+        marbleOreFrequency = c.get("ores", "MarbleFrequency", 1).getInt();
     }
 }
--- src/main/java/unstudio/chinacraft/world/gen/WorldGenListener.java
+++ src/main/java/unstudio/chinacraft/world/gen/WorldGenListener.java
@@ -4,17 +4,18 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.terraingen.OreGenEvent;
 import unstudio.chinacraft.common.ChinaCraft;
+import unstudio.chinacraft.common.config.FeatureConfig;
 
 /**
  * Created by Mouse on 2017/2/1.
  */
 public class WorldGenListener{
 
-    private final WorldGenCCOre WORLD_GEN_COPPER_ORE = new WorldGenCCOre(new int[]{0},20,64,0,8, ChinaCraft.copperOre);
-    private final WorldGenCCOre WORLD_GEN_TIN_ORE = new WorldGenCCOre(new int[]{0},10,64,0,8, ChinaCraft.tinOre);
-    private final WorldGenCCOre WORLD_GEN_SILVER_ORE = new WorldGenCCOre(new int[]{0},4,32,0,8, ChinaCraft.silverOre);
-    private final WorldGenCCOre WORLD_GEN_JADE_ORE = new WorldGenCCOre(new int[]{0},4,64,32,4, ChinaCraft.jadeOre);
-    private final WorldGenCCOre WORLD_GEN_MARBLE_ORE = new WorldGenCCOre(new int[]{0},1,128,32,48, ChinaCraft.blockMarble);
+    private final WorldGenCCOre WORLD_GEN_COPPER_ORE = new WorldGenCCOre(new int[]{0}, FeatureConfig.copperOreFrequency,64,0,8, ChinaCraft.copperOre);
+    private final WorldGenCCOre WORLD_GEN_TIN_ORE = new WorldGenCCOre(new int[]{0},FeatureConfig.tinOreFrequency,64,0,8, ChinaCraft.tinOre);
+    private final WorldGenCCOre WORLD_GEN_SILVER_ORE = new WorldGenCCOre(new int[]{0},FeatureConfig.silverOreFrequency,32,0,8, ChinaCraft.silverOre);
+    private final WorldGenCCOre WORLD_GEN_JADE_ORE = new WorldGenCCOre(new int[]{0},FeatureConfig.jadeOreFrequency,64,32,4, ChinaCraft.jadeOre);
+    private final WorldGenCCOre WORLD_GEN_MARBLE_ORE = new WorldGenCCOre(new int[]{0},FeatureConfig.marbleOreFrequency,128,32,48, ChinaCraft.blockMarble);
     private final WorldGenCCFlower WORLD_GEN_CC_FLOWER = new WorldGenCCFlower();
     private final WorldGenBambooShoot WORLD_GEN_BAMBOO_SHOOT = new WorldGenBambooShoot();
 
